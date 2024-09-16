9b032238421096ea0694bc5b115b28c639acf4b8
biettuot-cloud <nguyenha.lc13@gmail.com>
2024-09-16 13:59:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Ví dụ: Hiển thị thông báo chào mừng khi người dùng truy cập trang
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Welcome to 24h Tin Tức!');
+});
